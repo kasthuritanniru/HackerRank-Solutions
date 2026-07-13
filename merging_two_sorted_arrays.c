c8f774da1eaaa45c46f3e6684d4088615dd3e45f
kasthuritanniru <tannirukasthuri@gmail.com>
2026-07-13 22:23:47 +0530

    added solution

---
+++ merging_two_sorted_arrays.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    int m;
+    scanf("%d", &m);
+    int b[m];
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    int i = 0, j = 0, k = 0;
+    int c[1000];
+    while (i < n && j < m)
+    {
+        if (a[i] < b[j])
+        {
+            c[k++] = a[i++];
+        }
+        else
+        {
+            c[k++] = b[j++];
+        }
+    }
+    while (i < n)
+    {
+        c[k++] = a[i++];
+    }
+    while (j < m)
+    {
+        c[k++] = b[j++];
+    }
+    for (int i = 0; i < m + n; i++)
+    {
+        printf("%d ", c[i]);
+    }
+    return 0;
+}
